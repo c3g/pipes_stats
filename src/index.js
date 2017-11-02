c3ef16b040596b0c06de7633bb6c73597eaf522b
Rom Grk <romgrk.cc@gmail.com>
2017-11-02 13:04:21 -0400

    import fontawesome

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 
+import 'font-awesome/css/font-awesome.min.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'react-bootstrap-table/dist/react-bootstrap-table-all.min.css'
 import 'styles/global-styles'
