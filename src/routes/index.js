82da79211a6e0d6d197ceb182ec600fef6de4b38
Rom Grk <romgrk.cc@gmail.com>
2017-11-03 16:22:11 -0400

    remove unused var

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { AppContainer } from 'containers'
-import { Header } from 'components'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import styled from 'styled-components'
 
